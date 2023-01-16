3af4b14580a0e102b1a5a128326d3d5e24c8d17e
khushi <khushisuri9252gmail.com>
2023-01-16 23:17:35 +0530

    second

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -4,17 +4,27 @@ import NoteItem from '../components/NoteItem';
 import dummyNotes from '../dummy_notes'
 import {BsPlusLg} from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const Notes = ({notes}) => {
+  const [showSearch,setShowSearch]=useState(false);
+  const [text,setText]=useState('');
+  const [filteredNotes,setFilteredNotes]=useState(notes);
+
+  const handleSearch=()=>{
+    setFilteredNotes(notes.filter(note=>{
+      
+    }))
+  }
   return (
     <section>
         <header className="notes_header">
-            <h2>My Notes <button className='btn primary'><CiSearch/></button></h2>
-            {/* <input type="text" autoFocus placeholder='keyword...'/> */}
-            {/* <button className='btn'><CiSearch/></button> */}
+           {!showSearch && <h2>My Notes <button className='btn' onClick={()=>setShowSearch(prevState=> !prevState)}><CiSearch/></button></h2>}
+            { showSearch && <input type="text" value={text} onChange={(e)=>setText(e.target.value)}  autoFocus placeholder='keyword...'/> }
+            {/* <button className='btn' onClick={()=>setShowSearch(prevState=> !prevState)}><CiSearch/></button> */}
         </header>
         <div className='notes_container'>
     {
-        notes.map(note=><NoteItem key={note.id} note={note}/>)
+        filteredNotes.map(note=><NoteItem key={note.id} note={note}/>)
     }
         </div>
         <Link to="/create-note" className='btn add__btn'><BsPlusLg/></Link>
